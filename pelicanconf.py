f6c5183c0524b894a979cbfd0ba7ba28ba3b0e99
Jarmos-san <somraj.1994@gmail.com>
2021-01-13 16:46:20 +0530

    🐛 Fixed language issue with Pelican build

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,7 +9,7 @@ PATH = 'content'
 
 TIMEZONE = 'Asia/Calcutta'
 
-DEFAULT_LANG = 'English'
+DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -32,3 +32,5 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
+
+THEME = 'peng'
